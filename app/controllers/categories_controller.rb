7342430b6b2546ca692a0e475c44c28f273744a0
MatiasLyyra <matias.lyyra@gmail.com>
2016-09-30 17:19:53 +0300

    Categories index page shows now subcategories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
+
 class CategoriesController < ApplicationController
+  #encoding: utf-8
   before_action :set_category, only: [:show]
+  before_action :list_all_categories, :list_all_sub_categories, only: [:index]
 
   def index
-    @categories = Category.find_by_sql(["SELECT * FROM Category"])
   end
 
   def new
@@ -18,5 +20,20 @@ class CategoriesController < ApplicationController
 
   def set_category
     @category = Category.getCategoryById(params[:id])
+  end
+
+  def list_all_categories
+    @categories = Category.find_by_sql(["SELECT * FROM Category WHERE upper_category_id IS NULL"])
+  end
+
+  def list_all_sub_categories
+    subCategories = Category.find_by_sql(["SELECT * FROM Category WHERE upper_category_id IS NOT NULL"])
+    @allSubCategories = {}
+    subCategories.each do |subCategory|
+      if @allSubCategories[subCategory.upper_category_id].nil?
+        @allSubCategories[subCategory.upper_category_id] = Array.new
+      end
+      @allSubCategories[subCategory.upper_category_id].push(subCategory)
+    end
   end
 end

fb51095b1b5896539c246898f8870dcb5d8d0cad
Matias Lyyra <matias.lyyra@gmail.com>
2016-09-30 23:51:23 +0300

    Fixed indentation on some files (hopefully)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,21 @@ class UsersController < ApplicationController
 	end
 
 	def create
-	    if User.Validate?(user_params)
-	    	password_salt = BCrypt::Engine.generate_salt
-	    	password = BCrypt::Engine.hash_secret(user_params[:password], password_salt)
-	    	User.CreateUser(user_params[:nick], password, password_salt).first
-	    	#Whoa, used ActiveRecords find_by here, please don't crucify me. find_by_sql bugged for some reason here 4/5 times
-	    	@user = User.find_by(nick: user_params[:nick])
+		if User.Validate?(user_params)
+			password_salt = BCrypt::Engine.generate_salt
+			password = BCrypt::Engine.hash_secret(user_params[:password], password_salt)
+			User.CreateUser(user_params[:nick], password, password_salt).first
+			#Whoa, used ActiveRecords find_by here, please don't crucify me. find_by_sql bugged for some reason here 4/5 times
+			@user = User.find_by(nick: user_params[:nick])
 			redirect_to @user
-	    else
+		else
 			if not User.GetUserByNick(user_params[:nick]).nil?
 				flash[:notice] = 'Nick is already taken'
 			else
 				flash[:notice] = 'Invalid form'
 			end
 			redirect_to :back
-	    end
-	    
+		end
 	end
 
 	def show

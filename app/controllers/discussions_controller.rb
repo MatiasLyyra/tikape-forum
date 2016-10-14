fe489114fd8990e719e24f783448e22c745ed37b
Matias Lyyra <matias.lyyra@gmail.com>
2016-10-15 00:40:05 +0300

    Fixed links to discussions in category show and some other minor stuff
    -Added back link to discussion
    -Changed colors of the panels in discussion

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -3,7 +3,7 @@ class DiscussionsController < ApplicationController
   before_action :list_all_discussions, only: [:index]
   before_action :authenticate_user, only: [:new, :create]
   before_action :set_discussion, only: [:show]
-  before_action :set_category, only: [:show]
+  before_action :set_category, only: [:show, :create]
 
 
   def index
@@ -13,12 +13,12 @@ class DiscussionsController < ApplicationController
   end
 
   def create
-    if Discussion.validate?(discussion_params[:title], params[:category_id]) && discussion_params[:message] != nil
-      Discussion.createDiscussion(discussion_params[:title], params[:category_id])
+    if Discussion.validate?(discussion_params[:title], @category.id) && discussion_params[:message] != nil
+      Discussion.createDiscussion(discussion_params[:title], @category.id)
       #Dirty hack below.
       @discussion = Discussion.where(title: discussion_params[:title]).order("time DESC").first
       Message.createMessage(session[:user_id], discussion_params[:message], @discussion.id)
-      redirect_to discussion_path(@discussion.id)
+      redirect_to [@category,@discussion]
     else
       flash[:alert] = 'Invalid form'
       redirect_to :back
